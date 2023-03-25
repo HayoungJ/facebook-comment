6fc5dab9c31f299a5739fcbecd47cc07b8f15bf3
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-25 19:15:52 +0900

    ADD: static 게시글 레이아웃

---
+++ src/components/UserPost/UserPost.jsx
@@ -0,0 +1,31 @@
+import styles from './UserPost.module.scss'
+
+import profileImage from '../../assets/images/default_profile.png'
+
+const UserPost = () => {
+  return (
+    <article className={styles.post}>
+      <div className={styles.post__header}>
+        <img
+          className={styles.post__profile}
+          src={profileImage}
+          alt="profile"
+        />
+        <div className={styles.post__info}>
+          <h3 className={styles.post__name}>전하영</h3>
+          <span className={styles.post__time}>22시간 전</span>
+        </div>
+      </div>
+      <div className={styles.post__content}>
+        댓글/답글 남기기, 삭제하기, 좋아요 기능을 이용할 수 있습니다.
+        <br />
+        스타일은 페이스북을 모티브로 하고 있습니다.
+        <br />
+        작성한 댓글/답글/좋아요는 해당 브라우저에서만 확인할 수 있습니다.
+      </div>
+      <div className={styles['post__comments-count']}>댓글 n개</div>
+    </article>
+  )
+}
+
+export default UserPost
