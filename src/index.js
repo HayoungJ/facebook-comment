1d63738b281f31656adec660a3553ba2f1634eff
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-27 01:33:01 +0900

    ADD: 전역으로 답글 입력을 열고 닫는 context

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import { CommentsProvider } from './contexts/CommentContext';
+import { CommentsProvider } from './contexts/CommentsContext';
+import { RepliesProvider } from './contexts/RepliesContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CommentsProvider>
-      <App />
+      <RepliesProvider>
+        <App />
+      </RepliesProvider>
     </CommentsProvider>
   </React.StrictMode>
 );
