da3a6ade39c70d30acb8f2f5ed5a414ee50036be
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-27 18:00:10 +0900

    FIX: 삭제 메뉴 / 좋아요 기능 context 이용해 제어 ADD: 댓글/답글 삭제 기능 ADD: 댓글 시각 표시

--- src/components/UserComment/CommentList/CommentList.jsx
+++ src/components/UserComment/CommentList/CommentList.jsx
@@ -1,20 +1,19 @@
 import styles from './CommentList.module.scss';
+
 import profileImage from '../../../assets/images/default_profile.png';
-import { useState } from 'react';
+
 import CommentInput from '../CommentInput/CommentInput';
+
 import { useReplies } from '../../../contexts/RepliesContext';
+import { useLikes } from '../../../contexts/LikesContext';
+import { useMenu } from '../../../contexts/MenuContext';
+import { useComments } from '../../../contexts/CommentsContext';
 
 const CommentList = ({ comments, parent = null }) => {
-  const [likes, setLikes] = useState([]);
-
+  const [rawComments, setComments] = useComments();
+  const [likes, handleLike] = useLikes();
+  const [menu, setMenu] = useMenu();
   const [replies, setReplies, focusedReply, setFocusedReply] = useReplies();
-
-  const handleLike = (id) => {
-    let updatedLikes = [...likes];
-    if (!likes.includes(id)) updatedLikes.push(id);
-    else updatedLikes = updatedLikes.filter((like) => like !== id);
-    setLikes(updatedLikes);
-  };
 
   const handleReply = (id) => {
     let updatedReplies = [...replies];
@@ -24,6 +23,31 @@ const CommentList = ({ comments, parent = null }) => {
     updatedReplies.push(replyId);
     setReplies(updatedReplies);
     setFocusedReply(replyId);
+  };
+
+  const handleMenu = (id) => {
+    if (menu === id) setMenu(null);
+    else setMenu(id);
+  };
+
+  const handleDelete = (id) => {
+    const updatedComments = rawComments.filter(
+      (comment) => comment.id !== id && comment.parent !== id
+    );
+    setComments(updatedComments);
+  };
+
+  const getTimeDiff = (time) => {
+    const diff = new Date().getTime() - new Date(time).getTime();
+
+    let diffTime = Math.floor(diff / 1000);
+    if (diffTime < 60) return `${diffTime}초`;
+
+    diffTime = Math.floor(diff / (1000 * 60));
+    if (diffTime < 60) return `${diffTime}분`;
+
+    diffTime = Math.floor(diff / (1000 * 60 * 60));
+    return `${diffTime}시간`;
   };
 
   return (
@@ -38,7 +62,9 @@ const CommentList = ({ comments, parent = null }) => {
             />
             <div
               className={`${styles.comment__text} ${
-                likes.includes(comment.id) && styles['comment__text--like']
+                likes &&
+                likes.includes(comment.id) &&
+                styles['comment__text--like']
               } ${
                 focusedReply === comment.id && styles['comment__text--focused']
               }`}
@@ -46,17 +72,41 @@ const CommentList = ({ comments, parent = null }) => {
               <h4>{comment.user}</h4>
               <p>{comment.text}</p>
             </div>
+            <div className={styles.comment__delete}>
+              <button
+                onClick={() => handleMenu(comment.id)}
+                onBlur={() => handleMenu(null)}
+              >
+                <span>삭제</span>
+              </button>
+              <div className={styles.comment__guide}>삭제</div>
+              <ul
+                className={`${styles.comment__menu} ${
+                  menu === comment.id && styles['comment__menu--show']
+                }`}
+              >
+                <li onClick={() => handleDelete(comment.id)}>삭제</li>
+              </ul>
+            </div>
           </div>
-          <div className={styles.comment__buttons}>
+          <div className={styles.comment__bottoms}>
             <button
               className={`${
-                likes.includes(comment.id) && styles['comment__button--active']
+                likes &&
+                likes.includes(comment.id) &&
+                styles['comment__button--active']
               }`}
               onClick={() => handleLike(comment.id)}
             >
               좋아요
             </button>
             <button onClick={() => handleReply(comment.id)}>답글 달기</button>
+            <div className={styles.comment__time}>
+              {getTimeDiff(comment.createdTime)}
+              <div className={styles.comment__guide}>
+                {new Date(comment.createdTime).toLocaleString()}
+              </div>
+            </div>
           </div>
           {comment?.replies ? (
             <CommentList comments={comment.replies} parent={comment.id} />
