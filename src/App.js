0e1d3946073d45641693cb7d09cd030dbe4ebb83
Hayoung Jeon <hayoungj0710@gmail.com>
2023-03-28 20:24:12 +0900

    ADD: 프로젝트 내 이미지 출처 표시

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
+import { useState } from 'react';
 import './App.scss';
 import UserPost from './components/UserPost/UserPost';
 
 const App = () => {
+  const [showAuthors, setShowAuthors] = useState(false);
+
   return (
     <div className="App">
       <UserPost />
+      <div
+        className={`authors ${showAuthors && 'authors--show'}`}
+        onClick={() => setShowAuthors(!showAuthors)}
+      >
+        이미지 출처
+        <ul>
+          <li>
+            Icons made by
+            <a
+              href="https://www.flaticon.com/authors/kmg-design"
+              title="kmg design"
+            >
+              kmg design
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </li>
+          <li>
+            Icons made by
+            <a
+              href="https://www.flaticon.com/authors/md-tanvirul-haque"
+              title="Md Tanvirul Haque"
+            >
+              Md Tanvirul Haque
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </li>
+          <li>
+            Icons made by
+            <a
+              href="https://www.flaticon.com/kr/authors/md-tanvirul-haque"
+              title="Md Tanvirul Haque"
+            >
+              Md Tanvirul Haque
+            </a>
+            from
+            <a href="https://www.flaticon.com/kr/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </li>
+          <li>
+            Icons made by
+            <a
+              href="https://www.flaticon.com/authors/icon-wind"
+              title="icon wind"
+            >
+              icon wind
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
